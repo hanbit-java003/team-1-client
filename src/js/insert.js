f319d4878afab231c956d61a15bbf7f5e2bd7c5e
5004 <5004>
2017-09-25 15:46:33 +0900

    콕콕 입력 페이지 UI만 넣었습니다. common.less에 global-width 추가 했습니다.

--- src/js/insert.js
+++ src/js/insert.js
@@ -29,24 +29,36 @@ loadGoogleMapsApi().then(function(googleMaps) {
 
 $('.btn-menu-plus').on('click', function () {
     var html = require('../template/insert/menu-tag.hbs');
-    $('#background-preview').append(html);
+    $('.background-preview').append(html);
     menuDragEvent();
 });
 
 function menuDragEvent() {
     var x = 0, y = 0;
 
-    $('#background-preview > .menu-tag').on('dragstart', function (e) {
+    $('.background-preview > .menu-tag').on('dragstart', function (e) {
         console.log('start');
         x = e.pageX - x;
         y = e.pageY - y;
         console.log('page1 - ' + x + ' : ' + y );
     });
 
-    $('#background-preview > .menu-tag').on('dragend', function (e) {
+    $('.background-preview > .menu-tag').on('dragend', function (e) {
         console.log('end');
         x = e.pageX - x;
         y = e.pageY - y;
+        if (x < 0) {
+            x = 0;
+        }
+        if (y < 0) {
+            y = 0;
+        }
+        if (x > 460) {
+            x = 460;
+        }
+        if (y > 430) {
+            y = 430;
+        }
         $(this).css({
             'top': y + 'px',
             'left': x + 'px'
