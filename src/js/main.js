b41761db9f24947bc89cdf1f8008d84b7dbec112
kch <kch3338@naver.com>
2017-09-26 17:19:10 +0900

    main page ui & add pictures - ch

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,65 @@
 require('bootstrap');
 require('../less/main.less');
+
 var common = require('./common.js');
-
-var hello = require('./sample/hello');
-
-$('.say-hello').on('click', function () {
-    alert(hello.hello($('#txt-hello').val()));
-});
-
-$('.goto-sub').on('click', function () {
-    location.href = 'sub.html';
-});
 
 var loadGoogleMapsApi = require('load-google-maps-api-2');
 
-loadGoogleMapsApi.key = 'AIzaSyDmSxIrhoC4OAiGOtO6ddcFCwSMRbgfPGs';
-loadGoogleMapsApi.language = 'ko';
-loadGoogleMapsApi.version = '3';
+var cardContentsModel = require('./model/card-contents-list');
 
-loadGoogleMapsApi().then(function (googleMaps) {
-    console.log(googleMaps);
+function initMainMap() {
+    loadGoogleMapsApi.key = 'AIzaSyDmSxIrhoC4OAiGOtO6ddcFCwSMRbgfPGs';
+    loadGoogleMapsApi.language = 'ko';
+    loadGoogleMapsApi.version = '3';
 
-    var location = [
-        ['고라멘', 37.555344, 126.932692, 1],
-        ['미분당', 37.556792, 126.933534, 2],
-        ['조선의육개장', 37.557007, 126.935057, 3],
-        ['마루가메제면 신촌점', 37.555667, 126.936178, 4],
-        ['털보고된이 신촌점', 37.558257, 126.934696, 5],
-        ['크리스터치킨 신촌점', 37.558037, 126.934715, 6]
-    ];
+    loadGoogleMapsApi().then(function (googleMaps) {
+        // console.log(googleMaps);
 
-    var mapOptions = {
-        zoom: 16,
-        scrollwheel: false,
-        center: new googleMaps.LatLng(location[1][1], location[1][2])
-    };
+        var mapOptions = {
+            zoom: 16,
+            scrollwheel: false,
+            center: new googleMaps.LatLng(cardContentsModel[3].lat, cardContentsModel[3].lng)
+        };
 
-    var map = new googleMaps.Map($('#main-map')[0], mapOptions);
+        var map = new googleMaps.Map($('#main-map')[0], mapOptions);
+        var infowindow = new googleMaps.InfoWindow();
 
-    var infowindow = new googleMaps.InfoWindow();
+        var marker, i;
+        var spoon = '../img/marker-spoon.png';
 
-    var marker, i;
-    var spoon = '../img/marker-spoon.png';
+        for (i=0; i<cardContentsModel.length; i++) {
+            marker = new googleMaps.Marker({
+                position: new googleMaps.LatLng(cardContentsModel[i].lat, cardContentsModel[i].lng),
+                map: map,
+                icon: spoon
+            });
+            googleMaps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    infowindow.setContent(cardContentsModel[i].title);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
 
-    for (i=0; i<location.length; i++) {
-        marker = new googleMaps.Marker({
-            position: new googleMaps.LatLng(location[i][1], location[i][2]),
-            map: map,
-            title: location[i][0],
-            icon: spoon
-        });
-        googleMaps.event.addListener(marker, 'click', (function (marker, i) {
-            return function () {
-                infowindow.setContent(location[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
+initMainMap();
+
+function initCardContents(cardContentsModel) {
+    $('.card-contents-list').empty();
+
+    var template = require('../template/card-contents-list.hbs');
+
+    for (var i=0; i<cardContentsModel.length; i++) {
+        var cardHtml = template(cardContentsModel[i]);
+
+        $('.contents-nearby').append(cardHtml);
     }
-}).catch(function (error) {
-    console.error(error);
-});
+}
+
+initCardContents(cardContentsModel);
 
 $('.card-tab-btns > li').on('click', function () {
     if ($(this).hasClass('active')) {
