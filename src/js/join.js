7e4fe67f31f12bd19ab163e42e93e41a717c218d
jmk0629 <jmkjmk7410@naver.com>
2017-09-28 17:23:19 +0900

    join/join-food/setting page 수정 문기

--- src/js/join.js
+++ src/js/join.js
@@ -2,6 +2,30 @@ require('../less/join.less');
 
 var common = require('./common.js');
 
+$('#cock-join-email-btn').on('click', function () {
+    console.log('dsadasd');
+
+    var email = $('#cock-join-email').val().trim();
+
+    $.ajax({
+        url:'/api/sendMail/auth',
+        method:'POST',
+        data: {
+            email: email
+        },
+        success: function (result) {
+            console.log(result);
+        },
+        error: function (jqXHR) {
+            console.log('dsadsa');
+        }
+
+
+    });
+
+});
+
+
 
 $('.cock-join-btn-cancel').on('click', function () {
     location.href = './';
