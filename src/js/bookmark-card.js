7ddefc3634ef39e10760b33dc560abb5f934c474
rladbgh12 <rladbgh12@naver.com>
2017-09-29 11:09:55 +0900

    Bookmark UI 수정 중

--- src/js/bookmark-card.js
+++ src/js/bookmark-card.js
@@ -1,17 +1,50 @@
 module.exports = [{
+    id:'butantsu',
     thumbnail : './img/butantsu_sample.jpg',
-    storeName : '부탄츄 신촌점',
+    title : '부탄츄 신촌점',
     flag: '../img/flag/jp.png',
-    sort: '../img/sort/noodle_icon.jpg',
-    comment: '난 여기 맛있는데.... 지예가 싫다더라.... 고라멘의 라이벌',
-    like: '123',
-    article: '22'
+    foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    comment: '난 여기 맛있는데.... 지예가 싫다더라.... 고라멘의 라이벌'
 }, {
-    thumbnail : './img/go_ramen.jpg',
-    storeName : '고라멘 신촌점',
+    id:'goramen',
+    thumbnail : './img/main-img/goramen.jpg',
+    title : '고라멘 신촌점',
     flag: '../img/flag/jp.png',
-    sort: '../img/sort/noodle_icon.jpg',
-    comment: '지예가 추천했어요! 완전 강추! ...하지만 부탄츄의 라이벌',
-    like: '123',
-    article: '223'
+    foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    comment: '지예가 추천했어요! 완전 강추! ...하지만 부탄츄의 라이벌'
+}, {
+    id:'josun',
+    thumbnail : './img/main-img/josun.jpg',
+    title : '조선의 육개장 신촌점',
+    flag: '../img/main-img/national-flags/kr.png',
+    foodIcon: './img/main-img/food-icons/64 x 64-50.png',
+    comment: '아직 가보지 못한 미지의 세계...'
+}, {
+    id:'marugame',
+    thumbnail : './img/main-img/marugame.jpg',
+    title : '마루가메제면 신촌점',
+    flag: '../img/flag/jp.png',
+    foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    comment: ''
+}, {
+    id:'mibundang',
+    thumbnail : './img/main-img/mibundang.jpg',
+    title : '미분당 신촌점',
+    flag: '../img/main-img/national-flags/cn.png',
+    foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    comment: ''
+}, {
+    id:'fish',
+    thumbnail : './img/main-img/fish.jpg',
+    title : '털보고된이생선구이 신촌점',
+    flag: '../img/main-img/national-flags/kr.png',
+    foodIcon: './img/main-img/food-icons/64 x 64-44.png',
+    comment: ''
+}, {
+    id:'crister',
+    thumbnail : './img/main-img/chicken.jpg',
+    title : '크리스터치킨 신촌점',
+    flag: '../img/main-img/national-flags/kr.png',
+    foodIcon: './img/main-img/food-icons/64 x 64-27.png',
+    comment: ''
 }];
