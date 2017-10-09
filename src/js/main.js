079c247ad42075cfd8d9e3f3e8869e8053bb6929
kch3338 <kch3338@naver.com>
2017-10-09 20:35:46 +0900

    마우스오버 외 이것저것.. - ch

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,14 @@
 require('bootstrap');
 require('../less/main.less');
 
-var common = require('./common.js');
-
+var common = require('./common');
 var loadGoogleMapsApi = require('load-google-maps-api-2');
 
-// 주변 맛집 카드 모델
-var cardContentsNearby = require('./model/card-contents-nearby');
-// 추천 맛집 카드 모델
-var cardContentsRecommend = require('./model/card-contents-recommend');
+// 주변 맛집 모델
+var contentsNearby = require('./model/card-contents-nearby');
+// 추천 맛집 모델
+var contentsRecommend = require('./model/card-contents-recommend');
 
-// 인증키 - ch
 loadGoogleMapsApi.key = 'AIzaSyDmSxIrhoC4OAiGOtO6ddcFCwSMRbgfPGs';
 loadGoogleMapsApi.language = 'ko';
 loadGoogleMapsApi.version = '3';
@@ -67,9 +65,9 @@ function initMainMap(position) {
             var spoon = '../img/spoon_red.png';
 
             // 마커 추가
-            for (i = 0; i < cardContentsNearby.length; i++) {
+            for (i = 0; i < contentsNearby.length; i++) {
                 marker = new googleMaps.Marker({
-                    position: new googleMaps.LatLng(cardContentsNearby[i].lat, cardContentsNearby[i].lng),
+                    position: new googleMaps.LatLng(contentsNearby[i].lat, contentsNearby[i].lng),
                     map: map,
                     icon: spoon
                 });
@@ -77,11 +75,31 @@ function initMainMap(position) {
                 // 마커 클릭시 타이틀 팝업
                 googleMaps.event.addListener(marker, 'click', (function (marker, i) {
                     return function () {
-                        infowindow.setContent(cardContentsNearby[i].title);
+                        infowindow.setContent(contentsNearby[i].title);
                         infowindow.open(map, marker);
                     }
                 })(marker, i));
             }
+
+            var circles = new googleMaps.Circle({
+                center : mapOptions.center,
+                // 반지름 (m)
+                radius: 500,
+                // 외곽선 두께
+                strokeWeight: 1,
+                // 외곽선 색상
+                strokeColor: '#FF7E5F',
+                // 외곽선 불투명도 (0~1)
+                strokeOpacity: 0.7,
+                // 외곽선 스타일
+                strokeStyle: 'dashed',
+                // 원 내부 색상
+                fillColor: '#FF7E5F',
+                // 원 내부 불투명도 (0~1)
+                fillOpacity: 0.2
+            });
+
+            circles.setMap(map);
         }
         else if ($('#recommend-rest').hasClass('active')) {
             // 옵션을 따로 설정해서 mapOptions 변수에 담았음
@@ -91,7 +109,7 @@ function initMainMap(position) {
                 // 지도 스크롤 설정 off
                 scrollwheel: false,
                 // 지도 센터 설정
-                center: new googleMaps.LatLng(cardContentsRecommend[0].lat, cardContentsRecommend[0].lng)
+                center: new googleMaps.LatLng(contentsRecommend[0].lat, contentsRecommend[0].lng)
             };
 
             // 지도 생성
@@ -100,9 +118,9 @@ function initMainMap(position) {
             var spoon = '../img/spoon_blue.png';
 
             // 마커 추가
-            for (i = 0; i < cardContentsRecommend.length; i++) {
+            for (i = 0; i < contentsRecommend.length; i++) {
                 marker = new googleMaps.Marker({
-                    position: new googleMaps.LatLng(cardContentsRecommend[i].lat, cardContentsRecommend[i].lng),
+                    position: new googleMaps.LatLng(contentsRecommend[i].lat, contentsRecommend[i].lng),
                     map: map,
                     icon: spoon
                 });
@@ -110,7 +128,7 @@ function initMainMap(position) {
                 // 마커 클릭시 타이틀 팝업
                 googleMaps.event.addListener(marker, 'click', (function (marker, i) {
                     return function () {
-                        infowindow.setContent(cardContentsRecommend[i].title);
+                        infowindow.setContent(contentsRecommend[i].title);
                         infowindow.open(map, marker);
                     }
                 })(marker, i));
@@ -121,92 +139,101 @@ function initMainMap(position) {
     });
 }
 
-// 주변 맛집 리스트
-function initContentsNearby(cardContentsNearby) {
-    $('.contents-nearby').empty();
+function clkTab() {
+    // 주변 맛집 / 추천 맛집 버튼 클릭
+    $('.card-tab-btns > li').on('click', function () {
+        if ($(this).hasClass('active')) {
+            return;
+        }
 
-    var template = require('../template/main/card-contents-list.hbs');
+        var tabIndex = $(this).index();
 
-    for (var i = 0; i < cardContentsNearby.length; i++) {
-        var cardHtml = template(cardContentsNearby[i]);
+        // 주변 맛집 버튼[0] / 추천 맛집 버튼[1]
+        var tabBtns = $(this).parent('.card-tab-btns').find('li');
+        tabBtns.removeClass('active');
+        $(tabBtns[tabIndex]).addClass('active');
 
-        $('.contents-nearby').append(cardHtml);
-    }
+        // 주변 맛집 컨텐츠[0] / 추천 맛집 컨텐츠[1]
+        var tabContents = $(this).parents('.main-card-tab').find('.card-tab-contents > li');
+        tabContents.removeClass('active');
+        $(tabContents[tabIndex]).addClass('active');
 
-    // 리스트 클릭하면 상세 페이지로 이동
-    $('.card-contents-list > li').on('click', function () {
-        goDetail($(this).attr('uid'));
+        if ($('#nearby-rest').hasClass('active')) {
+            getLocation();
+        }
+        else {
+            initMainMap();
+        }
     });
 }
 
-initContentsNearby(cardContentsNearby);
+clkTab();
 
-// 추천 맛집 리스트
-function initContentsRecommend(cardContentsRecommend) {
-    $('.contents-recommend').empty();
-
-    var template = require('../template/main/card-contents-list.hbs');
-
-    for (var i = 0; i < cardContentsRecommend.length; i++) {
-        var cardHtml = template(cardContentsRecommend[i]);
-
-        $('.contents-recommend').append(cardHtml);
-    }
-
-}
-
-initContentsRecommend(cardContentsRecommend);
-
-function goDetail(uid) {
-    location.href = 'detail.html?uid=' + uid;
-}
-
-// 주변 맛집 / 추천 맛집 버튼 클릭
-$('.card-tab-btns > li').on('click', function () {
-    if ($(this).hasClass('active')) {
-        return;
-    }
-
-    var tabIndex = $(this).index();
-
-    // 주변 맛집 버튼[0] / 추천 맛집 버튼[1]
-    var tabBtns = $(this).parent('.card-tab-btns').find('li');
-    tabBtns.removeClass('active');
-    $(tabBtns[tabIndex]).addClass('active');
-
-    // 주변 맛집 컨텐츠[0] / 추천 맛집 컨텐츠[1]
-    var tabContents = $(this).parents('.main-card-tab').find('.card-tab-contents > li');
-    tabContents.removeClass('active');
-    $(tabContents[tabIndex]).addClass('active');
-
-    // 마커를 다시 찍기 위해 또 불러옴
-    if ($('#nearby-rest').hasClass('active')) {
-        getLocation();
-    }
-    else if ($('#recommend-rest').hasClass('active')) {
-        initMainMap();
-    }
-});
-
+// 정렬 기준
 function initSort() {
-    var template = require('../template/main/card-contents-sort.hbs');
+    var sortTpl = require('../template/main/card-contents-sort.hbs');
 
-    $('.card-contents-sort').html(template);
+    $('.card-contents-sort').html(sortTpl);
 
     clkSort();
 }
 
 initSort();
 
-
+// 정렬 기준 버튼 클릭 이벤트
 function clkSort() {
     $('.card-contents-sort div').on('click', function () {
+        if ($(this).hasClass('active')) {
+            return;
+        }
+
         $(this).parent('.card-contents-sort').find('div').removeClass('active');
         $(this).addClass('active');
-    })
+    });
 }
 
-// 즐겨찾기 클릭 이벤트
+// 주변 맛집 리스트
+function initNearby(contentsNearby) {
+    $('.contents-nearby').empty();
+
+    var contentsTpl = require('../template/main/card-contents-list.hbs');
+
+    for (var i = 0; i < contentsNearby.length; i++) {
+        var nearbyHtml = contentsTpl(contentsNearby[i]);
+
+        $('.contents-nearby').append(nearbyHtml);
+    }
+
+    // 리스트 클릭하면 상세 페이지로 이동
+    $('.card-contents-list > li').on('click', function () {
+        goDetail($(this).attr('uid'));
+    });
+
+    // clkFavorite();
+    // hoverContents();
+}
+
+initNearby(contentsNearby);
+
+// 추천 맛집 리스트
+function initRecommend(contentsRecommend) {
+    $('.contents-recommend').empty();
+
+    var contentsTpl = require('../template/main/card-contents-list.hbs');
+
+    for (var i = 0; i < contentsRecommend.length; i++) {
+        var recommendHtml = contentsTpl(contentsRecommend[i]);
+
+        $('.contents-recommend').append(recommendHtml);
+    }
+
+    // clkFavorite();
+    // hoverContents();
+}
+
+initRecommend(contentsRecommend);
+
+// 즐겨찾기 버튼 클릭 이벤트
 function clkFavorite() {
     $('.card-contents-favorite i').on('click', function (event) {
         event.stopPropagation();
@@ -219,13 +246,36 @@ function clkFavorite() {
         else {
             $(this).removeClass('fa-star');
             $(this).addClass('fa-star-o');
-            $(this).css('color', '#FF7E5F');
             alert('즐겨찾기에서 삭제되었습니다.');
         }
-    })
+    });
 }
 
 clkFavorite();
+
+function hoverContents() {
+    $('.card-contents-list > li').on('mouseenter', function () {
+        $(this).find('.card-contents').css('color', '#FF7E5F');
+
+        var location = $(this).find('.locationContainer');
+        var lat = location.attr('lat');
+        var lng = location.attr('lng');
+
+        var myLatLng = {lat: lat, lng: lng};
+
+        console.log(myLatLng);
+    });
+
+    $('.card-contents-list > li').on('mouseleave', function () {
+        $(this).find('.card-contents').css('color', '#5e5e5e');
+    });
+}
+
+// hoverContents();
+
+function goDetail(uid) {
+    location.href = 'detail.html?uid=' + uid;
+}
 
 $('#btn-join-test').on('click', function () {
     location.href = 'join.html';
@@ -234,4 +284,3 @@ $('#btn-join-test').on('click', function () {
 $('#btn-join-food-test').on('click', function () {
     location.href = 'join-food.html';
 });
-
