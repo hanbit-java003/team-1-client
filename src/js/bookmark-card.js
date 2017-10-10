1af0d246280dd1d74c3d27da8adf3f5a10533da4
rladbgh12 <rladbgh12@naver.com>
2017-10-10 18:30:34 +0900

    Bookmark UI 수정 중(지도 개별적 추가) - 김유호

--- src/js/bookmark-card.js
+++ src/js/bookmark-card.js
@@ -4,13 +4,17 @@ module.exports = [{
     title : '부탄츄 신촌점',
     flag: '../img/flag/jp.png',
     foodIcon: './img/main-img/food-icons/64 x 64-47.png',
-    comment: '난 여기 맛있는데.... 지예가 싫다더라.... 고라멘의 라이벌'
+    lat: 37.556841,
+    lng: 126.935082,
+    comment: '난 여기  맛있는데.... 지예가 싫다더라.... 고라멘의 라이벌'
 }, {
     id:'goramen',
     thumbnail : './img/main-img/goramen.jpg',
     title : '고라멘 신촌점',
     flag: '../img/flag/jp.png',
     foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    lat: 37.555344,
+    lng: 126.932692,
     comment: '지예가 추천했어요! 완전 강추! ...하지만 부탄츄의 라이벌'
 }, {
     id:'josun',
@@ -18,6 +22,8 @@ module.exports = [{
     title : '조선의 육개장 신촌점',
     flag: '../img/main-img/national-flags/kr.png',
     foodIcon: './img/main-img/food-icons/64 x 64-50.png',
+    lat: 37.557007,
+    lng: 126.935057,
     comment: '아직 가보지 못한 미지의 세계...'
 }, {
     id:'marugame',
@@ -25,6 +31,8 @@ module.exports = [{
     title : '마루가메제면 신촌점',
     flag: '../img/flag/jp.png',
     foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    lat: 37.555667,
+    lng: 126.936178,
     comment: ''
 }, {
     id:'mibundang',
@@ -32,6 +40,8 @@ module.exports = [{
     title : '미분당 신촌점',
     flag: '../img/main-img/national-flags/cn.png',
     foodIcon: './img/main-img/food-icons/64 x 64-47.png',
+    lat: 37.556792,
+    lng: 126.933534,
     comment: ''
 }, {
     id:'fish',
@@ -39,6 +49,8 @@ module.exports = [{
     title : '털보고된이생선구이 신촌점',
     flag: '../img/main-img/national-flags/kr.png',
     foodIcon: './img/main-img/food-icons/64 x 64-44.png',
+    lat: 37.558257,
+    lng: 126.934696,
     comment: ''
 }, {
     id:'crister',
@@ -46,5 +58,7 @@ module.exports = [{
     title : '크리스터치킨 신촌점',
     flag: '../img/main-img/national-flags/kr.png',
     foodIcon: './img/main-img/food-icons/64 x 64-27.png',
+    lat: 37.558037,
+    lng: 126.934715,
     comment: ''
 }];
--- src/js/bookmark.js
+++ src/js/bookmark.js
@@ -2,33 +2,83 @@ require('../less/bookmark.less');
 require('bootstrap');
 require('./main.js');
 
-var model = require('./bookmark-card');
+var loadGoogleMapsApi = require('load-google-maps-api-2');
+
+loadGoogleMapsApi.key = 'AIzaSyBItgKQoeoJUNrQiMPHXXMiSqXOBKw1w0w';
+loadGoogleMapsApi.language = 'ko';
+loadGoogleMapsApi.version = '3';
+
+var bookmarkModel = require('./bookmark-card');
 // model은 bookmark-card.js
 var template = require('../template/bookmark-card.hbs');
 
 function initBookmark() {
 
-    for (var i=0; i<model.length; i++) {
+    for (var i=0; i<bookmarkModel.length; i++) {
 
-        var html = template(model[i]);
+        var html = template(bookmarkModel[i]);
 
         $('.bookmark-container').append(html);
     }
 
     // Bookmark 버튼 클릭시 변환
     $('.bookmark-btn').on('click', function () {
-        if($(this).hasClass('fa-bookmark')) {
-            $(this).removeClass('fa-bookmark').addClass('fa-bookmark-o');
+        if($(this).hasClass('fa-star')) {
+            $(this).removeClass('fa-star').addClass('fa-star-o');
             $(this).css('color', '#ff8300');
+            alert("즐겨찾기 목록에서 제거되었습니다.")
         }
-        else if ($(this).hasClass('fa-bookmark-o')) {
-            $(this).removeClass('fa-bookmark-o').addClass('fa-bookmark');
+        else if ($(this).hasClass('fa-star-o')) {
+            $(this).removeClass('fa-star-o').addClass('fa-star');
             $(this).css('color', '#ff8300');
+            alert("즐겨찾기 목록에 추가되었습니다.")
+            /*이 알람이 필요할런지 모르겠지만 일단은 추가함.*/
         }
     });
 }
 
 initBookmark();
 
+function initBookmarkMap() {
 
+    loadGoogleMapsApi().then(function (googleMaps) {
+
+        var infowindow = new googleMaps.InfoWindow();
+        var marker;
+        var i;
+
+        var mapOptions = {
+            zoom: 18,
+            scrollwheel: true,
+            center:
+                new googleMaps.LatLng(bookmarkModel[0].lat, bookmarkModel[0].lng)
+        };
+
+        var bookmarkMaps = new googleMaps.Map($('.bookmark-map')[0], mapOptions);
+
+        var spoonMark = '../img/spoon_red.png';
+
+        for (i=0; i<bookmarkModel.length; i++) {
+            marker = new googleMaps.Marker({
+                position: new googleMaps.LatLng(bookmarkModel[i].lat, bookmarkModel[i].lng),
+                map: bookmarkMaps,
+                icon: spoonMark
+            });
+
+            googleMaps.event.addListener(marker, 'click', (function (marker, i) {
+                return function () {
+                    infowindow.setContent(bookmarkModel[i].title);
+                    infowindow.open(bookmarkMaps, marker);
+                }
+            })(marker, i));
+        }
+
+    }).catch(function (error) {
+        console.error(error);
+    });
+
+}
+
+
+initBookmarkMap();
 
