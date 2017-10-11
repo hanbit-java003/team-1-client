f9771428d3241b639ea767345efe47bc167d373d
jmk0629 <jmkjmk7410@naver.com>
2017-10-11 10:47:53 +0900

    test-login page 삭제
    좌측 상단 버튼 클릭시 로그인창 나오게끔 만듦.
    member-layer 만듦.

--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,7 @@
 require('../less/common.less');
-require('./test-login.js');
+require('../less/member-layer.less');
 
 
-
-
-/*$('.header-btn-member').on('click', function () {
-    location.href = './setting.html';
-});*/
 
 $('.header-logo').on('click', function () {
     location.href = './';
@@ -32,3 +27,111 @@ function searchBarToggle() {
 }
 
 searchBarToggle();
+
+$('.header-btn-member').on('click', function () {
+    $.ajax({
+        url: '/api/member/get',
+        success: function (result) {
+            openMemberLayer(result);
+        }
+    });
+
+
+    function openMemberLayer(memberInfo) {
+        $('body').append('<div class="overlay-layer dark-layer"></div>');
+        $('body').css('overflow', 'hidden');
+        var memberLayerTemplate = require('../template/member-layer.hbs');
+        var memberLayer = memberLayerTemplate(memberInfo);
+
+        $('body').append(memberLayer);
+
+        $('.cock-member-layer').animate({
+            left: '0px' // 몇으로 바꿀건지.
+        },{
+            duration: 500,
+            complete:function () {
+                if(!memberInfo.signedIn){
+                    console.log('dasdas');
+                    $('#cock-login-btn').on('click', function () {
+                        signIn();
+                    });
+                }
+                else {
+                    $('#cock-logout').on('click', function () {
+                        signOut();
+                    });
+                    $('#cock-setting').on('click', function () {
+                        location.href = '../setting.html';
+                    });
+                }
+            }
+        });
+
+        $('.overlay-layer').on('click', function () {
+           closeMemberLayer();
+        });
+
+    }
+});
+
+
+
+function signOut() {
+    $.ajax({
+        url: '/api/member/signout',
+        success:function () {
+            location.href='../';
+        }
+    });
+}
+
+function signIn(){
+    var email = $('#cock-login-email').val().trim();
+    var pw = $('#cock-login-pw').val().trim();
+    var remember = $('#cock-login-remember').prop('checked');
+
+    if (!email){
+        alert('이메일을 입력하세요.');
+        $('#cock-login-email').focus();
+        return;
+    }
+    else if(!pw) {
+        alert('비밀번호를 입력하세요.');
+        $('#cock-login-pw').focus();
+        return;
+    }
+
+    $.ajax({
+        url:'/api/member/signin',
+        method: 'POST',
+        data: {
+            email: email,
+            password: pw,
+            remember: remember
+        },
+        success: function (result) {
+            alert('로그인 성공');
+            location.href = './';
+        },
+        error: function (jqXHR) {
+            alert(jqXHR.responseJSON.message());
+        }
+    })
+}
+
+function closeMemberLayer(callback){
+    $('.cock-member-layer').animate({
+        left: '-333px'
+    }, {
+        duration:500,
+        complete : function () {
+            $('.ht-member-layer').remove(); // 위 상단 메뉴 클릭시 나오는 메뉴 사라짐
+            $('.overlay-layer').remove();  // 엘리먼트를 없애버린다.속성
+            $('body').css('overflow', 'auto');  // 히든에서 클릭으로 속성을 바꿈.
+
+            if (typeof callback === 'function'){ // callback이 없을수도? 함수가 아닐수도 있다. 함수는 function인지?
+                callback();
+            }
+        }
+    });
+}
