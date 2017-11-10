9c8acb33d2fac29dff238125720b06fbdbb71707
jmk0629 <jmkjmk7410@naver.com>
2017-11-10 18:00:40 +0900

    main 즐겨찾기 단 잠깐 만짐.

--- src/js/main.js
+++ src/js/main.js
@@ -303,6 +303,7 @@ function clkFavorite() {
             }
             else {
                 signedInFavorite();
+                cockSignedInFavorite();
             }
         }
     });
@@ -318,10 +319,22 @@ function signedOutFavorite() {
     });
 }
 
+// 로그인 상태에서 즐겨찾기 가져오기
+function cockSignedInFavorite() {
+    $.ajax({
+        url: '/api/cock/member/bookmark',
+        success: function (result) {
+            $(this).removeClass('fa-star-o');
+            $(this).addClass('fa-star');
+        }
+    });
+}
+
 // 로그인 상태의 즐겨찾기 클릭 이벤트
 function signedInFavorite() {
     $('.card-contents-favorite i').on('click', function (event) {
         event.stopPropagation();
+
 
         if ($(this).hasClass('fa-star-o')) {
             $(this).removeClass('fa-star-o');
