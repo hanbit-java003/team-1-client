7277f8795d2edbd252eaa742499c2926b4ee18dd
nahcoos <hscsc515@gmail.com>
2017-11-13 11:56:41 +0900

    좋아요 기능만, view 는 아직.  - pull x

--- src/js/report.js
+++ src/js/report.js
@@ -2,3 +2,11 @@ require('bootstrap');
 require('../less/report.less');
 
 var common = require('./common');
+
+$('.report-btn-cancel').on('click', function () {
+    history.back();
+});
+
+$('.report-btn-save').unbind('click').on('click', function () {
+    console.log('저장');
+});
