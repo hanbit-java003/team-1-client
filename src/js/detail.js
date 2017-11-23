c1d4db90dd19758631d2b479504fcaf9b82571a0
nahcoos <hscsc515@gmail.com>
2017-11-23 14:52:51 +0900

    음식 사진 이벤트 추가중 - 풀 ㄴㄴ해

--- src/js/detail.js
+++ src/js/detail.js
@@ -262,8 +262,6 @@ function initContents(restaurant) {
     initRestInfo(restaurant);
     initLikes();
 
-    initImgs();
-
     sortBtn();
     settingBtn();
 
@@ -280,51 +278,39 @@ function initContents(restaurant) {
     // 신고 버튼
     $('.food-report').unbind('click').on('click', function () {
         var articleId = $(this).parents('.content-wrapper').attr('articleId');
-        location.href = './report.html?rid='+ rid +'&articleId='+ articleId;
+        location.href = './report.html?rid=' + rid + '&articleId=' + articleId;
     });
 
     // 사진 클릭
     $('.food-img-tag').unbind('click').on('click', function () {
-        if ($(this).parent().find($('.detail-menu-tag')).hasClass('visible')) {
-            $(this).parent().find($('.detail-menu-tag')).css('visibility', 'hidden');
-            $(this).parent().find($('.detail-menu-tag')).removeClass('visible');
-        }
-        else {
-            $(this).parent().find($('.detail-menu-tag')).css('visibility', 'visible');
-            $(this).parent().find($('.detail-menu-tag')).addClass('visible');
-        }
+        showMenuTags($(this));
+        showNextImg($(this));
     });
 }
 
-function initImgs() {
-    $(document).ready(function () {
-        var current = 0;
-        var slide_length = $('.slide_ul>li').length;//이미지의 갯수를 변수로
-        var btn_ul = '<ul class="slide_btn"></ul>';//버튼 LIST 작성할 UL
+function showMenuTags(img) {
+    if (img.parent().find($('.detail-menu-tag')).hasClass('visible')) {
+        img.parent().find($('.detail-menu-tag')).css('visibility', 'hidden');
+        img.parent().find($('.detail-menu-tag')).removeClass('visible');
+    }
+    else {
+        img.parent().find($('.detail-menu-tag')).css('visibility', 'visible');
+        img.parent().find($('.detail-menu-tag')).addClass('visible');
+    }
+}
 
-        $('.slide_ul>li').hide();//이미지 안보이게
-        $('.slide_ul>li').first().show();//이미지 하나만 보이게
+function showNextImg(img) {
+    img.on('click', function () {
+        var size = $(this).find('li').length;
+        var li = $(this).find('li.active');
+        var index = li.index();
 
-        $(btn_ul).prependTo($('.slide'));//slide 클래스위에 생성
-        for (var i = 0; i < slide_length; i++) {//동그라미 버튼 생성 이미지 li 개수 만큼
-            var child = '<li><a href="#none">' + i + '</a></li>';
-            $(child).appendTo($('.slide_btn'));
+        index++;
+        if (index >= size) {
+            index = 0;
         }
-
-        $('.slide_btn > li > a').first().addClass('active');
-        $('.slide_btn > li > a').on('click', slide_stop);
-
-//버튼 클릭시 호출되는 함수
-        function slide_stop() {
-            var fade_idx = $(this).parent().index();
-            current = $(this).parent().index();//클릭한 버튼의 Index 를 받아서 그 다음 이미지부터 슬라이드 재생.
-            if ($('.slide_ul > li:animated').length >= 1) return false; //버튼 반복 클릭시 딜레이 방지
-            $('.slide_ul > li').fadeOut(10);
-            $('.slide_ul > li').eq(fade_idx).fadeIn(10);
-            $('.slide_btn > li > a').removeClass('active');
-            $(this).addClass('active');
-
-        }
+        $(this).find('li.active').removeClass('active');
+        $(this).find('li:eq(' + index + ')').addClass('active');
     });
 }
 
